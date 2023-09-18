409dd57fd0b67f958598525979b3f5d5c2a010fb
ltt <82834307+lnonsensel@users.noreply.github.com>
2023-09-18 18:15:56 +0300

    Update cfg.py

--- wireguardLib/cfg.py
+++ wireguardLib/cfg.py
@@ -1,9 +1,12 @@
 from dotenv import dotenv_values
 
-dotenvValues = dotenv_values("/mnt/c/Users/LTT's PC/Desktop/python/wireGuard4All/wireguardLib/.env")
+#define path to .env
+dotenvValues = dotenv_values("")
+
 SERVER_PRIVATE_KEY = dotenvValues['SERVER_PRIVATE_KEY']
 SERVER_PUBLIC_KEY = dotenvValues['SERVER_PUBLIC_KEY']
 
 SERVER_IP = dotenvValues['SERVER_IP']
 
-CFG_PATH = "/mnt/c/Users/LTT's PC/Desktop/python/wireGuard4All/wireguardLib/config"
+#define path to cfg
+CFG_PATH = ""
